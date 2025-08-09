8adca057e5473204f2a3a96f061979a784d015df
nafisat akokhia <naphydeby@gmail.com>
2025-08-09 21:43:12 +0100

    adjusted hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,15 +2,14 @@
 import React from 'react'
 import {  ChevronRight } from "lucide-react";
 
-// import { Button } from './ui/button';
-import Link from 'next/link';
+import { Button } from './ui/button';
 const Hero = () => {
    
   return (
-    <section className='bg-cover bg-center bg-no-repeat h-screen  md:py-20 ' style={{ backgroundImage: 'url(/image/basil.jpg)' }} id='home'>
-    <div className='container mx-auto px-2 py-18 text-center'>
-        <div className='flex flex-col space-y-10 md:space-y-12   '>
-                <h1 className='text-3xl md:text-8xl lg:text-7xl  font-bold '>Natural Healing Through
+    <section className='bg-cover bg-center bg-no-repeat min-h-screen py-12 px4  md:py-20 ' style={{ backgroundImage: 'url(/image/basil.jpg)' }} id='home'>
+    <div className='container mx-auto px-2  text-center'>
+        <div className='flex flex-col space-y-8 md:space-y-12   '>
+                <h1 className='text-4xl sm:text-5xl md:text-7xl  font-bold leading-tight '>Natural Healing Through
                  <span className='text-green-600 font-bold animate-pulse'> Herbal  Wisdom</span>
                 </h1>
             <p className='text-2xl text-green-600 '>
@@ -18,10 +17,10 @@ const Hero = () => {
               expert consultations, and sustainable healing solutions for your journey to natural health.
             </p>
             <div className=' flex flex-col md:flex-row justify-center  gap-4   '>
-                <Link href='#herbs'  className='bg-green-600 hover:bg-green-500 text-lg px-8 py-4 rounded-md text-white font-semibold transition-all duration-300 transform hover:scale-105 flex cursor-pointer'> Explore Our Herbs <ChevronRight className="ml-2 h-5 w-5 mt-1  " /></Link>
-                <Link href='#contact' className='bg-white border-1 border-green-600 hover:bg-green-50 text-lg px-8 py-4 rounded-md text-green-600 font-semibold cursor-pointer'>Book Consultation</Link>
-               {/* <Button   className='bg-green-600 hover:bg-green-500 text-lg px-8 py-4 h-12 rounded-md text-white font-semibold transition-all duration-300 transform hover:scale-105 flex cursor-pointer'> Explore Our Herbs <ChevronRight className="ml-2 h-5 w-5 mt-1  " /></Button>
-                <Button  className='bg-white border-1 border-green-600 hover:bg-green-50 text-lg px-8 py-4 h-12 rounded-md text-green-600 font-semibold cursor-pointer'>Book Consultation</Button> */}
+                {/* <Link href='#herbs'  className='bg-green-600 hover:bg-green-500 text-lg px-8 py-4 rounded-md text-white font-semibold transition-all duration-300 transform hover:scale-105 flex cursor-pointer'> Explore Our Herbs <ChevronRight className="ml-2 h-5 w-5 mt-1  " /></Link>
+                <Link href='#contact' className='bg-white border-1 border-green-600 hover:bg-green-50 text-lg px-8 py-4 rounded-md text-green-600 font-semibold cursor-pointer'>Book Consultation</Link> */}
+               <Button   className='bg-green-600 hover:bg-green-500 text-lg px-8 py-4 h-12 rounded-md text-white font-semibold transition-all duration-300 transform hover:scale-105  flex items-center justify-center cursor-pointer'> Explore Our Herbs <ChevronRight className="ml-2 h-5 w-5 mt-1  " /></Button>
+                <Button  className='bg-white border-1 border-green-600 hover:bg-green-50 text-lg px-8 py-4 h-12 rounded-md text-green-600 font-semibold cursor-pointer'>Book Consultation</Button>
 
             </div>
         </div>
